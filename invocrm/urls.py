4476a5c13c3902ae15c338c8994ca2b082add341
Tamerlan Abilov <tamerlan.abilov94@gmail.com>
2021-04-17 18:14:22 +0400

    add example templates

--- invocrm/urls.py
+++ invocrm/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(('api.urls', 'api'), namespace='api')),
+    path('index/', lambda r: render(r, 'index.html')),
+    path('detail/', lambda r: render(r, 'detail.html')),
 ]
