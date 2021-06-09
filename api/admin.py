1bdda8a2bb222cbc9b60bc2be98a103e685a6c61
Tamerlan Abilov <tamerlan.abilov94@gmail.com>
2021-06-09 09:58:58 +0300

    admin panel models

--- api/admin.py
+++ api/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 
 
 from api.main_models.annex import UnitOfMeasure
-from api.main_models.contract import TradeAgreement
+from api.main_models.contract import TradeAgreement, BaseContract, Contact, Bank, BankAccount, Company
 from api.models import CustomUser, Person
 
 admin.site.register(CustomUser)
 admin.site.register(Person)
 admin.site.register(TradeAgreement)
+admin.site.register(BaseContract)
+admin.site.register(Contact)
+admin.site.register(Bank)
+admin.site.register(BankAccount)
+admin.site.register(Company)
+
 admin.site.register(UnitOfMeasure)
