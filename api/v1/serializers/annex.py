a0c1bc5ddb801924f2892c27ce40d92b73e60c60
timabilov <tamerlan.abilov95@gmail.com>
2021-06-14 11:49:14 +0400

    sales manager for agent and rent contr annexes

--- api/v1/serializers/annex.py
+++ api/v1/serializers/annex.py
@@ -102,7 +102,7 @@ class AgentAnnexCreateSerializer(serializers.ModelSerializer):
 
         model = BaseAnnex
 
-        fields = ['contract', 'created', 'agent_items', 'with_vat', 'total']
+        fields = ['contract', 'created', 'agent_items', 'with_vat', 'total', 'sales_manager']
 
     def create(self, validated_data):
 
@@ -137,7 +137,7 @@ class RentAnnexCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BaseAnnex
-        fields = ['contract', 'created', 'rent_items', 'rent_conditions', 'with_vat', 'total']
+        fields = ['contract', 'created', 'rent_items', 'rent_conditions', 'with_vat', 'total', 'sales_manager']
 
     def create(self, validated_data):
 
